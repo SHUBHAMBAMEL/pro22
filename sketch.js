48140cdd9c96fdf52cc432c48fc306fe247805c9
SHUBHAMBAMEL <77445292+SHUBHAMBAMEL@users.noreply.github.com>
2021-02-05 10:03:24 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+	
+}
+
+
+function draw() {
+  background(bgImg);
+
+  drawSprites(); 
+  if keyPressed
+
+}
+
+function keyPressed() {
+	
+	// if you want to move right you have click on rightarrow and leftarrow.
+	if (keyDown('right')){
+		fairy.velocity.x =   +1;
+	}
+
+	// if you want to move left you have click on leftarrow and righttarrow.
+
+	if (keyDown('left')){
+		fairy.velocity.x =   -1;
+	}
+	
+
+}
